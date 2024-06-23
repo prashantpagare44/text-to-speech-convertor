c52ca7625d8fc61db3c69c3d5ef6059eb7bd602a
prashant pagare <prashantpagarejd@gmail.com>
2024-06-24 00:02:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let speech = new SpeechSynthesisUtterance();
+
+    let voices = [];
+    let voicesSelect = document.querySelector("select");
+
+    window.speechSynthesis.onvoiceschanged = () => {
+      voices = window.speechSynthesis.getVoices();
+      speech.voice = voices[0];
+
+      voices.forEach((voice, i) => {
+        let option = document.createElement("option");
+        option.value = i;
+        option.textContent = voice.name;
+        voicesSelect.appendChild(option);
+      });
+    };
+
+    voicesSelect.addEventListener("change", () => {
+      speech.voice = voices[voicesSelect.value];
+    });
+
+    document.querySelector("button").addEventListener("click", () => {
+      speech.text = document.querySelector("textarea").value;
+      window.speechSynthesis.speak(speech);
+    });
